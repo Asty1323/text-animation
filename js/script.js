328193bfc3e9cc4640b441228d64bfce3c232dc5
Asty1323 <astykhider@gmail.com>
2026-09-24 11:26:40 +0200

    first

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 // Husk fra dag 2: skriv "use strict" herunder
-
+"use strict"; 
 
 // Eksempel: vi henter ordet "word" ved hjælp af dets id-attribut
 const getWordElem = document.getElementById("word");
 
 // Skriv selv: hent knappen "zoomBtn" på samme måde, ved hjælp af dens id. Variablen skal hedde getZoomBtn
-
+const getZoomBtn = document.getElementById("zoomBtn")
 
 // Eksempel: vi lytter efter klik på knappen og kører en anonym function, når der klikkes - ligesom i billedskift-opgaven
 getZoomBtn.addEventListener("click", function() {
 
     // Skriv if/else-strukturen selv herinde, ligesom i billedskift-opgaven.
-    //
+    if (getWordElem.style.fontSize=true )
     // Nyt i dag: getWordElem.style.fontSize ændrer en CSS-egenskab (fontSize) direkte via JavaScript,
     // på samme måde som getImage.src ændrede et billede i billedskift-opgaven.
     //
